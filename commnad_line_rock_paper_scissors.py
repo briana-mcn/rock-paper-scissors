7d422cda7c463ffe7074dd96fedf04276535f5f7
briana-mcn <briana.mcn@gmail.com>
2017-06-15 16:51:10 -0500

    add function for help with user reprompt in main

--- commnad_line_rock_paper_scissors.py
+++ commnad_line_rock_paper_scissors.py
@@ -15,19 +15,32 @@ def main():
         game.rounds = input('How many odd rounds would you like to play? ')
 
     while not game.is_complete:
-        choices = ', or '.join(
-            [
-                ', '.join(game.valid_choices[:-1]),
-                game.valid_choices[-1]
-            ]
-        )
-        human.choice = input('Choose {}: '.format(choices))
-        computer.choice = random.choice(game.valid_choices)
+        validate_users_choice()
+
         game.print_players_menu('Input')
         game.play()
         game.print_players_menu('Score')
 
     game.determine_winner()
+
+
+def validate_users_choice():
+    choices = ', or '.join(
+        [
+            ', '.join(game.valid_choices[:-1]),
+            game.valid_choices[-1]
+        ]
+    )
+
+    p1 = input('Choose {}: '.format(choices))
+    p2 = random.choice(game.valid_choices)
+
+    for (player, choice) in ((game.player1, p1), (game.player2, p2)):
+        try:
+            player.choice = game.convert_input_to_move(choice)
+        except Exception:
+            print('Input from {} is invalid, enter another choice'.format(player))
+            return validate_users_choice()
 
 if __name__ == '__main__':
     main()
