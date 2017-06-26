0531f7a70adae6361bf7949bac5d4c8564205af0
briana-mcn <briana.mcn@gmail.com>
2017-06-26 10:09:45 -0500

    create unique exception classes for program

---
+++ rock_paper_scissors/exc.py
@@ -0,0 +1,49 @@
+class ProgramError(Exception):
+    """Base exception class for errors raised by rock paper scissors program."""
+    pass
+
+
+class UserDefinedError(ProgramError):
+    """Raised for all user input errors."""
+    pass
+
+
+class UserChoiceInputError(UserDefinedError):
+    """Raised for invalid User sting input."""
+    def __init__(self, message):
+        self.message = message
+
+
+class UserRoundsInputError(UserDefinedError):
+    """Raised for invalid rounds input."""
+    pass
+
+
+class RoundsStringError(UserRoundsInputError):
+    """Raised if rounds input is a string."""
+    def __init__(self, message):
+        self.message = message
+
+
+class RoundsEvenError(UserRoundsInputError):
+    """Raised if rounds input is even."""
+    def __init__(self, message):
+        self.message = message
+
+
+class RoundsNegativeError(UserRoundsInputError):
+    """Raised if rounds input is less than 0."""
+    def __init__(self, message):
+        self.message = message
+
+
+class RoundsGreaterThan100(UserRoundsInputError):
+    """Raised if rounds input is greater than 100."""
+    def __init__(self, message):
+        self.message = message
+
+
+class InstanceNotMoveError(ProgramError):
+    """Raised if conversion from user input to Move instance failed."""
+    def __init__(self, message):
+        self.message = message
