fe919c90fad91ccbf8280ea95440e4790ba20a63
briana-mcn <briana.mcn@gmail.com>
2017-06-27 12:29:53 -0500

    remove specific invalid rounds subclasses of InvalidRoundsError class

--- rock_paper_scissors/exc.py
+++ rock_paper_scissors/exc.py
@@ -18,31 +18,7 @@ class InvalidRoundsError(InputError):
     pass
 
 
-class RoundsStringError(UserRoundsInputError):
-    """Raised if rounds input is a string."""
-    def __init__(self, message):
-        self.message = message
-
-
-class RoundsEvenError(UserRoundsInputError):
-    """Raised if rounds input is even."""
-    def __init__(self, message):
-        self.message = message
-
-
-class RoundsNegativeError(UserRoundsInputError):
-    """Raised if rounds input is less than 0."""
-    def __init__(self, message):
-        self.message = message
-
-
-class RoundsGreaterThan100(UserRoundsInputError):
-    """Raised if rounds input is greater than 100."""
-    def __init__(self, message):
-        self.message = message
-
-
-class InstanceNotMoveError(ProgramError):
+class InstanceNotMoveError(RockPaperScissorsError):
     """Raised if conversion from user input to Move instance failed."""
     def __init__(self, message):
         self.message = message
